5c9e78ce19f495554cff317943e5d54847076bd3
mateusboemer <mattboemer@gmail.com>
2024-03-19 16:36:48 -0400

    Generate's deck

--- src/Game.py
+++ src/Game.py
@@ -1,4 +1,11 @@
+from random import shuffle
+
 class Game():
     def __init__(self):
-        pass
-    
+        deck = [(value, suit) for value in 
+                range(self.CARD_MIN_MAX[0], self.CARD_MIN_MAX[1]) 
+                for suit in self.SUITS]
+        shuffle(deck)
+
+        self.trump = deck[0]
+        self.deck = deck
