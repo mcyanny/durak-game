0fc05e3c8e8080ca2adbe26974072baa668cf7e7
mateusboemer <mattboemer@gmail.com>
2024-03-19 16:42:18 -0400

    Fixed deck generation

--- src/Game.py
+++ src/Game.py
@@ -1,10 +1,17 @@
 from random import shuffle
+from typing import *
 
 class Game():
-    def __init__(self):
+    def __init__(self, num_players_arg: int = None, 
+                 SUITS: Tuple[str] = ('♥', '♦', '♣', '♠'),
+                 CARDS_PER_HAND: int = 6,
+                 CARDS_MIN_MAX: Tuple[int, int] = (6, 15),
+                 PLAYERS_MIN_MAX: Tuple[int, int] = (2, 5)):
+
         deck = [(value, suit) for value in 
                 range(self.CARD_MIN_MAX[0], self.CARD_MIN_MAX[1]) 
                 for suit in self.SUITS]
+
         shuffle(deck)
 
         self.trump = deck[0]
