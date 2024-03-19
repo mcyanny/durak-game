abed9901d970af4fa5271f2acac8b927c7bc268b
Yan Mazheika <moyanchik1@gmail.com>
2024-03-19 16:52:58 -0400

    yes

--- src/Game.py
+++ src/Game.py
@@ -1,18 +1,25 @@
 from random import shuffle
-from typing import *
+from typing import Tuple
 
-class Game():
-    def __init__(self, num_players_arg: int = None, 
+class Game:
+    def __init__(self, num_players_arg: int = None,
                  SUITS: Tuple[str] = ('♥', '♦', '♣', '♠'),
                  CARDS_PER_HAND: int = 6,
-                 CARDS_MIN_MAX: Tuple[int, int] = (6, 15),
+                 CARD_MIN_MAX: Tuple[int, int] = (6, 15),  # Fixed the variable name here
                  PLAYERS_MIN_MAX: Tuple[int, int] = (2, 5)):
 
-        deck = [(value, suit) for value in 
-                range(self.CARD_MIN_MAX[0], self.CARD_MIN_MAX[1]) 
-                for suit in self.SUITS]
+        self.deck = [(value, suit) for value in 
+                     range(CARD_MIN_MAX[0], CARD_MIN_MAX[1]) 
+                     for suit in SUITS]
 
-        shuffle(deck)
+        shuffle(self.deck)  # Shuffling the deck properly
+        
+        
+        
+        
 
-        self.trump = deck[0]
-        self.deck = deck
+    
+    def initState(self):
+        trump = self.deck[0]  # Assigning the trump properly
+        state = State(deck, trump, players)
+
