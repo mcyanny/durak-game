8e711e6b951ef681e74c1311d505bed30a0aa4e3
Yan Mazheika <moyanchik1@gmail.com>
2024-03-19 20:24:24 -0400

    fdadsfd

--- src/State.py
+++ src/State.py
@@ -2,57 +2,60 @@ from typing import *
 from Player import Player
 
 card = Tuple[int, str]
-deck = List[card]
+deck_type = List[card]
+
 
 class State():
-    def __init__(self, deck: deck, trump: card):
+    def __init__(self, deck: deck_type, trump: card):
         self.deck = deck
         self.trump = trump
-<<<<<<< HEAD
+        self.trump_suit = trump[1]
         self.floor = []
-        self.hands = {}
+        self.hands = {} #key: player, value: list of cards
         self.players = []
-        self.player_roles = {'curr_attacker': None, 'second_attacker': None, 'defender': None}
+        self.player_roles = {'attacker': None, 'second_attacker': None, 'defender': None}
+        self.deal_cards()
+        
 
+    def deal_cards(self):
+        """draws cards til 6 for each player"""
+        for player in self.players:
+            while (len(self.hands[player]) < 6):
+                self.hands[player].append(self.deck.pop())
+        
     
     def set_players(self, players_arg: List[Player]):
         """
         Sets self.players to players_arg
+        FROM GAME CLASS
         
         Param: 
         players_arg: List[Player]
         """
         self.players = players_arg
 
+    def set_player_roles(self):
+        if (self.player_roles['attacker'] == None): #start of game, picks attacker with the lowest trump card
+            pass
+        else:
 
-    def deal_cards(self):
-        """
-        Sets self.hands as dictionary with a Player reference as a key 
-        and the player_hand as the value. Current_attacker is set to the 
-        player with the lowest value trump card.
-        """
-        hands = {player: [] for player in self.players}
-        trump_suit = self.trump[1]
-        lowest_trump = (15, trump_suit)
 
-        for _ in range(self.CARDS_PER_HAND):
-            for player_num in range(1, self.num_players + 1):
-                card = self.deck.pop()
-                hands[player_num].append(card)
-                if (card[1] == trump_suit) and (card[0] < lowest_trump):
-                    lowest_trump = card
-                    self.current_attacker = player_num
-        self.hands = hands
+                
+    def draw_cards(self):
+        """draws cards til 6 for each player"""
+        for player in self.players:
+            while (len(self.hands[player]) < 6):
+                self.hands[player].append(self.deck.pop())
 
 
     def get_stage(self) -> Player:
+        #TODO
         pass
 
 
     def reset_floor():
+        #TODO
         pass
-=======
         self.players = []
     
         
->>>>>>> b00a8cc1e455b296f77be09f80745e858a67910a
