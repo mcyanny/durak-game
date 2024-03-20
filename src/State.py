5a815633cb79bc5df6a03279e4f7b81f26115c27
Yan Mazheika <moyanchik1@gmail.com>
2024-03-19 21:31:57 -0400

    jfkdsafkdsf

--- src/State.py
+++ src/State.py
@@ -15,17 +15,33 @@ class State():
         self.players = []
         self.player_roles = {'attacker': None, 'second_attacker': None, 'defender': None}
 
-        
 
     def deal_cards(self):
         """draws cards til 6 for each player"""
+        first_player = self.players[0]
+        lowest_trump = 15
+        
         for player in self.players:
             self.hands[player] = []
-            while (len(self.hands[player]) < 6):
-                self.hands[player].append(self.deck.pop())
+            while len(self.hands[player]) < 6:
+                new_card = self.deck.pop()
+                if new_card[1] == self.trump_suit and new_card[0] < lowest_trump:
+                    first_player = player
+                self.hands[player].append(new_card)
         
+        if len(self.players) > 2:
+            attacker_index = self.players.index(first_player)
+            self.player_roles['attacker'] = first_player
+            self.player_roles['defender'] = self.players[(attacker_index + 1) % len(self.players)]
+            self.player_roles['second_attacker'] = self.players[(attacker_index + 2) % len(self.players)]  
+               
+        else:
+            attacker_index = self.players.index(first_player)
+            self.player_roles['attacker'] = first_player
+            self.player_roles['defender'] = self.players[(attacker_index + 1) % len(self.players)]
+
     
-    def set_players(self, players_arg: List[Player]):
+    def set_players(self, players_arg: List[Player]): # only at the beginning
         """
         Sets self.players to players_arg
         FROM GAME CLASS
@@ -34,13 +50,6 @@ class State():
         players_arg: List[Player]
         """
         self.players = players_arg
-        
-
-    def set_player_roles(self):
-        if (self.player_roles['attacker'] == None): #start of game, picks attacker with the lowest trump card
-            pass
-        else:
-            pass
 
 
                 
