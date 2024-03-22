e642fa768a7bdc235c7584e284f4ecb15fcd8196
mateusboemer <mattboemer@gmail.com>
2024-03-21 20:07:28 -0400

    Created all tests in state_test. Fixed bugs in state that led to the wrong first player being chosen, and fixed bugs in draw_cards that would lead to exceptions.

--- src/State.py
+++ src/State.py
@@ -18,19 +18,27 @@ class State():
         # TODO merge deal and draw
 
     """INIT METHODS (run once)"""
-    def deal_cards(self):
+    def initialize_hands(self):
         """draws cards til 6 for each player"""
         first_player = self.players[0]
         lowest_trump = 15
         
+        # gives each player their hand
         for player in self.players:
             self.hands[player] = []
             while len(self.hands[player]) < 6:
                 new_card = self.deck.pop()
-                if new_card[1] == self.trump_suit and new_card[0] < lowest_trump:
+                if (new_card[1] == self.trump_suit) and (new_card[0] < lowest_trump):
                     first_player = player
+                    lowest_trump = new_card[0]
                 self.hands[player].append(new_card)
         
+        # TODO find a way to choose the first player without a trump, or deal again
+        if lowest_trump == 15:
+            raise ValueError("You got unlucky with the draw.")
+        
+        # sets player_roles according to who has the lowest value trump
+        # card in their hand
         if len(self.players) > 2:
             attacker_index = self.players.index(first_player)
             self.player_roles['attacker'] = first_player
@@ -41,6 +49,7 @@ class State():
             attacker_index = self.players.index(first_player)
             self.player_roles['attacker'] = first_player
             self.player_roles['defender'] = self.players[(attacker_index + 1) % len(self.players)]
+
 
     def set_players(self, players_arg: List[Player]): # only at the beginning
         """
@@ -57,24 +66,36 @@ class State():
         """Returns the player_roles dictionary"""
         return self.player_roles
     
+    
     def get_floor(self):
         """Returns the floor"""
         return self.floor
+    
+    
+    def set_player_hand(self, player: Player, hand: deck_type):
+        self.hands[player] = hand
 
 
     """DRAW CARDS"""
     def draw_cards(self):
         """draws cards til 6 for each player"""
-        draw(self.player_roles['attacker'])
-        draw(self.player_roles['second_attacker'])
-        others = self.player_roles['others']
-        for player in others:
-            draw(others)
-        draw(self.player_roles['defender'])
         
         def draw(player):
             while (len(self.hands[player]) < 6 and len(self.deck) > 0):
                 self.hands[player].append(self.deck.pop())
+        
+        draw(self.player_roles['attacker'])
+        
+        if len(self.players) > 2:
+            draw(self.player_roles['second_attacker'])
+        
+        # TODO needs to make sure that they're drawing in the order that they gave the cards to the defender
+        others = self.player_roles['others']
+        for player in others:
+            if player != None:
+                draw(player)
+            
+        draw(self.player_roles['defender'])
 
 
     def get_stage(self) -> Player:
--- tests/State_test.py
+++ tests/State_test.py
@@ -1,28 +1,75 @@
 import unittest
+from typing import * 
+from random import randint
 
 import sys
 import os
 project_root = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.abspath(os.path.join(project_root, '..')))
 
-from ..src.Game import Game
-from ..src.State import State
+from src.Game import Game
+from src.State import State
 
 class TestGame(unittest.TestCase):
-    def set_up(self):
+    def setUp(self):
         self.game = Game()
         self.state = self.game.init_state()
         self.players = self.game.init_players(self.state)
-        self.state.deal_cards()
 
-    def test_game_init(self):
+    def test_initialize_hands(self):
         # Test initialization with default number of players
+        state = self.game.init_state()
+        players = self.game.init_players(state)
+        state.initialize_hands()
+        
+        # checks that hands are correct length
+        for player in players:
+            self.assertEqual(len(state.hands[player]), 6)
+            
+        # checks that each card is, in fact, a card
+        print(f"ALL PLAYERS: {players}")
+        hands = state.hands
+        print(hands)
+        # find out who has the lowest trump card in their hand
+        attacker = players[0]
+        lowest_trump = 15
+        trump_suit = state.trump_suit
+        for player, hand in hands.items():
+            for card in hand:
+                if (card[1] == trump_suit) and (card[0] < lowest_trump):
+                    lowest_trump = card[0]
+                    attacker = player
+                self.assertIsInstance(card, Tuple)
+
+        # checks that the attacker, defender, and second_attacker were set correctly
+        player_roles = state.get_player_roles()
+        attacker_index = players.index(attacker)
+        self.assertEqual(player_roles['attacker'], attacker)
+        self.assertEqual(player_roles['defender'], players[(attacker_index + 1) % len(players)])
+        if len(players) > 2:
+            self.assertEqual(player_roles['second_attacker'], players[(attacker_index + 2) % len(players)])
+
+
+    def test_draw_cards(self):
         game = Game()
-        self.assertEqual(game.get_num_players(), 4)
+        state = game.init_state()
+        players = game.init_players(state)
+        state.initialize_hands()
+        
+        # generates between 0 and 6 random cards
+        def generate_0_to_6_cards(suits):
+            return [(randint(6,15), suits[randint(0,3)]) for _ in range(0, randint(1, 6))]
 
+        # sets each player's hand to a random hand with between 0 and 6 cards
+        for player in players:
+            state.hands[player] = generate_0_to_6_cards(game.SUITS)
 
-    def test_state_init(self):
-        pass
+        # each player draws their cards
+        state.draw_cards()
+        
+        # checks that each player's hand is the correct length
+        for hand in state.hands.values():
+            self.assertEqual(len(hand), 6)
 
 
     def test_players_init(self):
